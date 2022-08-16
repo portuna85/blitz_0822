41df8b5c876fe85f00d50977af8cd20ea8402aa7
portuna85 <portuna85@gmail.com>
2022-08-17 01:46:37 +0900

    Update

---
+++ src/main/java/com/blitz/BlitzApplication.java
@@ -0,0 +1,13 @@
+package com.blitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlitzApplication.class, args);
+    }
+
+}
